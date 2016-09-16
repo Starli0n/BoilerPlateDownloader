dbc5b0e4c226cc23d4264e1a20e7b39df715b64f
Starli0n <Starli0n@users.noreply.github.com>
2016-09-16 14:53:49 +0200

    Add some comments for the doc

--- src/api/downloader.php
+++ src/api/downloader.php
@@ -1,39 +1,71 @@
 <?php
+/**
+ * BoilerPlateDownloader
+ *
+ * @package    BoilerPlateDownloader
+ * @subpackage Api
+ */
 
 namespace BoilerPlateDownloader\Api;
 
+/**
+ * Class to manage downloads on the server side
+ *
+ * @package    BoilerPlateDownloader
+ * @subpackage Api
+ * @see        src/api/routes.php
+ */
 class Downloader
 {
     /**
+     * Full path of the download directory
+     *
      * @var string
      */
     private $downloadPath;
 
     /**
+     * Relative path of the download directory
+     *
      * @var string
      */
     private $downloadDirectory;
 
     /**
+     * Relative path of the file to download
+     *
      * @var string
      */
     private $downloadFile;
 
     /**
+     * Extension added to the file to download
+     *
      * @var string
      */
     private $extension;
 
     /**
+     * URL of the file to download must start with 'http' or 'ftp'
+     *
      * @var string
      */
     private $fileUrl;
 
     /**
+     * Full path of the file to download
+     *
      * @var string
      */
     private $filePath;
 
+    /**
+     * Create a new Downloader
+     *
+     * @param string $downloadPath      Full path of the download directory
+     * @param string $downloadDirectory Relative path of the download directory
+     * @param string $extension         Extension added to the file to download
+     */
     public function __construct(string $downloadPath, string $downloadDirectory, string $extension)
     {
         $this->downloadPath = $downloadPath;
@@ -41,12 +73,26 @@ class Downloader
         $this->extension = $extension;
     }
 
+    /**
+     * Check whether a string is starting with a pattern
+     *
+     * @param string $haystack Original string to check
+     * @param string $needle   Pattern to verify
+     * @return bool True if $haystack starts with $needle, false otherwise
+     */
     public function startsWith(string $haystack, string $needle): bool
     {
         // search backwards starting from haystack length characters from the end
         return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
     }
 
+    /**
+     * Check whether a string is ending with a pattern
+     *
+     * @param string $haystack Original string to check
+     * @param string $needle   Pattern to verify
+     * @return bool True if $haystack ends with $needle, false otherwise
+     */
     public function endsWith($haystack, $needle): bool
     {
         // search forward starting from end minus needle length characters
@@ -54,6 +100,12 @@ class Downloader
             || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
     }
 
+    /**
+     * Set the file URL
+     *
+     * @param string $fileUrl URL of the file to download must start with 'http' or 'ftp'
+     * @return bool True if the setting was Ok, false otherwise
+     */
     public function setFileUrl(string $fileUrl): bool
     {
         $this->fileUrl = $fileUrl;
@@ -69,6 +121,13 @@ class Downloader
         return false;
     }
 
+    /**
+     * Donwload the remote file
+     *
+     * Effectively download the file previously set by 'setFileUrl()'
+     *
+     * @return bool True if the download was Ok, false otherwise
+     */
     public function downloadRemoteFile(): bool
     {
         try {
@@ -79,6 +138,11 @@ class Downloader
         return file_put_contents($this->filePath, $content);
     }
 
+    /**
+     * List all the files downloaded on the server
+     *
+     * @return array The list of the files
+     */
     public function listDir(): array
     {
         $files = scandir($this->downloadPath);
@@ -88,6 +152,11 @@ class Downloader
         return array_values($files);
     }
 
+    /**
+     * Delete the selected files downloaded
+     *
+     * @param array The list of the files to delete
+     */
     public function deleteFiles(array $files)
     {
         foreach ($files as $file) {
@@ -96,11 +165,21 @@ class Downloader
         unset($file);
     }
 
+    /**
+     * Getter $downloadDirectory
+     *
+     * @return string $downloadDirectory
+     */
     public function getDownloadDirectory(): string
     {
         return $this->downloadDirectory;
     }
 
+    /**
+     * Getter $downloadFile
+     *
+     * @return string $downloadFile
+     */
     public function getDownloadFile(): string
     {
         return $this->downloadFile;
--- src/api/router.php
+++ src/api/router.php
@@ -1,15 +1,49 @@
 <?php
+/**
+ * BoilerPlateDownloader
+ *
+ * @package    BoilerPlateDownloader
+ * @subpackage Api
+ */
 
 namespace BoilerPlateDownloader\Api;
 
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+/**
+ * Controller to handle routes
+ *
+ * Each function is a callback which implements this signature
+ * <code>
+ *     public function callback(Request $request, Response $response, array $args): Response
+ * </code>
+ *
+ * @package    BoilerPlateDownloader
+ * @subpackage Api
+ * @see        src/api/routes.php
+ */
 class Router
 {
+    /**
+     * Logger
+     *
+     * @var Logger
+     */
     private $logger;
+
+    /**
+     * Downloader
+     *
+     * @var Downloader
+     */
     private $api;
 
+    /**
+     * Create a new Router controller
+     *
+     * @param \Interop\Container\ContainerInterface $container ContainerInterface
+     */
     public function __construct(\Interop\Container\ContainerInterface $container)
     {
         $this->logger = $container->logger;
@@ -17,6 +51,15 @@ class Router
     }
 
     /**
+     * [GET] Basic route to test the routing
+     *
+     * The Response is a json message "Hello $name" with the attribute 'name' of the request
+     *
+     * @param Request   $request  The current Request object
+     * @param Response  $response The current Response object
+     * @param Response  $args     The additional arguments
+     * @return Response The updated Response object in (json)
+     *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function hello(Request $request, Response $response, array $args): Response
@@ -32,6 +75,17 @@ class Router
     }
 
     /**
+     * [GET] List all the files downloaded by the server
+     *
+     * The Response contains a json message with the attribute:
+     * > directory: Relative directory where the files are stored
+     * > files:     List of files
+     *
+     * @param Request   $request  The current Request object
+     * @param Response  $response The current Response object
+     * @param Response  $args     The additional arguments
+     * @return Response The updated Response object in (json)
+     *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function listFiles(Request $request, Response $response, array $args): Response
@@ -47,6 +101,20 @@ class Router
     }
 
     /**
+     * [PUT] Run a download request of a file
+     *
+     * The Request contains a json message with the attribute:
+     * > file: The file to download, must start with http or ftp
+     *
+     * The Response contains a json message with the attribute:
+     * > directory: Relative directory where the files are stored
+     * > files:     List of files
+     *
+     * @param Request   $request  The current Request object
+     * @param Response  $response The current Response object
+     * @param Response  $args     The additional arguments
+     * @return Response The updated Response object in (json)
+     *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function download(Request $request, Response $response, array $args): Response
@@ -78,6 +146,20 @@ class Router
     }
 
     /**
+     * [DELETE] Delete selected files
+     *
+     * @param Request   $request  The current Request object
+     * @param Response  $response The current Response object
+     * @param Response  $args     The additional arguments
+     * @return Response The updated Response object in (json)
+     *
+     * The Request contains a json message with the attribute:
+     * > files: The list a the files to delete
+     *
+     * The Response contains a json message with the attribute:
+     * > directory: Relative directory where the files are stored
+     * > files:     List of files
+     *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function delete(Request $request, Response $response, array $args): Response
